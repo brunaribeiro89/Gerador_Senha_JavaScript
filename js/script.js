f2d6a19755185e3e7149779975956c5b20a53c6d
Bruna Ribeiro <bruna.ribeiro@brf.com>
2022-09-14 08:53:38 -0300

    gerador senhas javascript

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatedPasswordElement = document.querySelector("#generated-password")
+
+
+//Funções
+const getLetterLowerCase= () => {
+   return String.fromCharCode(Math.floor(Math.random()*26)+97)
+}
+const getLetterUpperCase= () => {
+    return String.fromCharCode(Math.floor(Math.random()*26)+65)
+ }
+const getNumber = () => {
+  return Math.floor(Math.random()*10).toString()
+}
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.;!@#$%&*+-?^~"
+    return symbols[Math.floor(Math.random()*symbols.length)]
+  }
+  
+const generatePassword = ( getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol) => {
+   let password = ""
+   const passwordLenght = 8
+   const generators = [ getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol]
+
+   for(i = 0 ; i < passwordLenght; i = i + generators.length) {
+    console.log(i)
+    generators.forEach(() => {
+            const randomValue = generators[ Math.floor(Math.random()*generators.length)]()
+            console.log(randomValue)
+            password += randomValue
+            
+        });
+      
+   }
+   console.log(password)
+   generatedPasswordElement.style.display = "block"
+   generatedPasswordElement.querySelector("h4").innerText = password
+}
+
+//Eventos
+generatePasswordButton.addEventListener("click", ()=> {
+    generatePassword(getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol)
+})
